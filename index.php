a5d15b6d3c4c5ab775415224bfc9e33847a30cac
Nabila2201010344 <127836877+Nabila2201010344@users.noreply.github.com>
2023-03-14 17:04:31 +0800

    sesi1

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan HTML sesi 1</title>
+</head>
+<body>
+  
+    <h1 style="text-align: center;"> BELAJAR WEB PROGRAMING<?="<sup>80</sup>"?></h1>
+
+    <script>
+        document.write("<h3>Menggunaksn script javascript</h3>");
+    </script>
+
+    <?php
+       echo"<h3>munggunakan script PHP</h3>";
+    ?>
+</body>
+</html>
